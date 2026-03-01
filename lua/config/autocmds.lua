6f2c70d659ff86862c00ff86c950078f9717b1ef
Vagif Mammadaliyev <vagif@vagif-mammadaliyev.local>
2026-03-01 17:38:45 +0400

    trailing new line fix try
    not fixed anything actually

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Always ensure files end with a newline on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.bo.endofline = true
+  end,
+})
