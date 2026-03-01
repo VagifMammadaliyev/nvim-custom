110a33642210bc00420c46931e0ad4f9ab7048a1
Vagif Mammadaliyev <vagif@vagif-mammadaliyev.local>
2026-03-01 17:37:53 +0400

    nvim tree width

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -37,7 +37,7 @@ return {
           sorter = "case_sensitive",
         },
         view = {
-          width = 30,
+          width = 45,
           relativenumber = false,
         },
         renderer = {
