5da0964e4343e6c91e14e5903903c0eba65c32c2
Vagif Mammadaliyev <vagif@vagif-mammadaliyev.local>
2026-03-11 04:47:52 +0400

    vague theme

--- lua/plugins/colorscheme.lua.lua
+++ lua/plugins/colorscheme.lua.lua
@@ -13,26 +13,42 @@
 --   end,
 -- }
 
+-- return
+--   {
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     priority = 1000,
+--     config = function()
+--       require("rose-pine").setup({
+--         variant = "moon",
+--         dark_variant = "moon",
+--         transparent = true, -- Set to true if you want transparent background
+--         styles = {
+--           italic = false,
+--         },
+--       })
+--       vim.cmd.colorscheme("rose-pine")
+--       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--       vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+--       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--       vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+--       vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+--     end,
+--   },
+-- }
+
 return {
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
+    "vague-theme/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
     config = function()
-      require("rose-pine").setup({
-        variant = "moon",
-        dark_variant = "moon",
+      require("vague").setup({
         transparent = true, -- Set to true if you want transparent background
-        styles = {
-          italic = false,
-        },
+        bold = false,
+        italic = false,
       })
-      vim.cmd.colorscheme("rose-pine")
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+      vim.cmd("colorscheme vague")
     end,
   },
 }
