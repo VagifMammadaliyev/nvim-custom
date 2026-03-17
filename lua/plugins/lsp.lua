2b6ddb828b3c69df8e56234d44ef8fd98c490902
Vagif Mammadaliyev <vagif@vagif-mammadaliyev.local>
2026-03-18 03:26:45 +0400

    lsp js / docker compose

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,18 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        vtsls = {
+          enabled = true,
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+          },
+        },
+        docker_compose_language_service = {
+          enabled = true,
+        },
         pyright = {
           enabled = true,
           settings = {
@@ -11,12 +23,12 @@ return {
                 autoSearchPaths = true,
                 useLibraryCodeForTypes = true,
                 diagnosticMode = "workspace",
-                typeCheckingMode = "basic"
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+                typeCheckingMode = "basic",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
