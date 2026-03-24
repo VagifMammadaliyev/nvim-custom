ecbac3f20753f781f3b4d71ad2dc3b4c873010c9
Vagif Mammadaliyev <vagif@vagif-mammadaliyev.local>
2026-03-25 01:51:38 +0400

    github theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -55,18 +55,14 @@
 
 return {
   {
-    "snesjhon/gitlab-nvim-theme",
-    lazy = false,
-    priority = 1000,
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("gitlab-theme").setup({
-        contrast = false,
-        borders = true,
-        italic = false,
-        bold = false,
-        transparent = true,
-      })
-      vim.cmd.colorscheme("gitlab_dark")
+      require("github-theme").setup({})
+
+      vim.cmd("colorscheme github_dark_tritanopia")
     end,
   },
 }
